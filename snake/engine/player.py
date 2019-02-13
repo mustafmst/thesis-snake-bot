757b938482876a44b991cfe9f8270632a7e23acc
Paweł <pawel.mstowski@gmail.com>
2019-02-13 19:24:40 +0100

    move files and organize code

---
+++ snake/engine/player.py
@@ -0,0 +1,18 @@
+
+
+class Player:
+    x = 10
+    y = 10
+    speed = 1
+
+    def moveRight(self):
+        self.x = self.x + self.speed
+    
+    def moveLeft(self):
+        self.x = self.x - self.speed
+
+    def moveUp(self):
+        self.y = self.y - self.speed
+
+    def moveDown(self):
+        self.y = self.y + self.speed
