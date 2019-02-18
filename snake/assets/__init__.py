eb0d98a00215a7a987098a68f065a1014274c16b
Paweł <pawel.mstowski@gmail.com>
2019-02-18 18:46:56 +0100

    Create assets file name getter and test for it

---
+++ snake/assets/__init__.py
@@ -0,0 +1,12 @@
+from os import path, getcwd
+
+FRUIT = 'fruit.png'
+SNAKE_BODY = 'snake_body.png'
+SNAKE_HEAD = 'snake_head.png'
+
+RELATIVE_PATH = 'snake/assets'
+
+__EXECUTION_PATH = getcwd()
+
+def get_absolute_file_path(file_name):
+    return path.join(__EXECUTION_PATH, RELATIVE_PATH, file_name)
