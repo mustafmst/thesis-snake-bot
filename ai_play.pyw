09d84701e481f4f4d41322cd37174e14dc683955
Paweł <pawel.mstowski@gmail.com>
2019-04-09 18:04:59 +0200

    Add random neural network generation and builder

---
+++ ai_play.pyw
@@ -0,0 +1,16 @@
+from geneticAI.neuralNetworks.random import NeuralNetworkBuilder
+import json
+
+builder = NeuralNetworkBuilder()
+
+builder.with_input_shape((2, 2))
+builder.with_dense_Layer(16)
+
+model = builder.build()
+
+with open('weights.json', 'w') as output:
+    weights = []
+    for i in range(1, len(model.layers)):
+        lw = model.get_layer(index=i).get_weights()
+        weights.append([lw[0].tolist(), lw[1].tolist()])
+    json.dump(weights, output, indent=4)
---
+++ geneticAI/neuralNetworks/random.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+import tensorflow as tf
+
+
+class NeuralNetworkBuilder:
+    def __init__(self):
+        self.model = keras.Sequential()
+
+    def with_input_shape(self, shape):
+        self.model.add(keras.layers.Flatten(input_shape=shape))
+
+    def with_dense_Layer(self, size):
+        self.model.add(keras.layers.Dense(size,
+                                          kernel_initializer='random_uniform',
+                                          bias_initializer='random_uniform'))
+
+    def build(self):
+        self.model.add(keras.layers.Dense(4, activation=tf.nn.softmax,
+                                          bias_initializer='random_uniform'))
+        self.model.compile(optimizer=tf.train.AdamOptimizer(),
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['accuracy'])
+        return self.model
